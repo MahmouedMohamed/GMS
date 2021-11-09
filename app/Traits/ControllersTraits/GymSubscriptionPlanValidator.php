be674d376129b247ef7ef31fe6b723262130a224
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-11-09 15:07:53 +0200

    Added Gym Subscription Data Validation

--- app/Traits/ControllersTraits/GymSubscriptionPlanValidator.php
+++ app/Traits/ControllersTraits/GymSubscriptionPlanValidator.php
@@ -20,5 +20,29 @@ trait GymSubscriptionPlanValidator
         if (!$user)
             throw new GymSubscriptionPlanNotFound();
         return $user;
+
+    public function validateGymSubscriptionPlanData(Request $request, String $related)
+    {
+        $rules = null;
+        switch ($related) {
+            case 'store':
+                $rules = [
+                    'numberOfMonths' => 'required|integer|min:1',
+                    'cost' => 'required|numeric|min:1',
+                    'discount' => 'required|numeric|min:0|max:100',
+                ];
+                break;
+            case 'update':
+                $rules = [
+                    'numberOfMonths' => 'required|integer|min:1',
+                    'cost' => 'required|numeric|min:1',
+                    'discount' => 'required|numeric|min:0|max:100',
+                ];
+                break;
+        }
+        $messages = [];
+        if ($request['language'] != null)
+            $messages = $this->getValidatorMessagesBasedOnLanguage($request['language']);
+        return Validator::make($request->all(), $rules, $messages);
     }
 }
