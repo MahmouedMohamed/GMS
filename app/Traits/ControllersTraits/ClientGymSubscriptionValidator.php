4a6871e26bf68db558d4188c9b7b862adc636c64
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-11-09 15:14:01 +0200

    Added Client Gym Subscription Validator

---
+++ app/Traits/ControllersTraits/ClientGymSubscriptionValidator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Traits\ControllersTraits;
+
+use App\Exceptions\ClientAlreadySubscribedToGymSubscription;
+use App\Exceptions\ClientGymSubscriptionNotFound;
+use App\Models\User;
+use App\Traits\ValidatorLanguagesSupport;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+trait ClientGymSubscriptionValidator
+{
+    use ValidatorLanguagesSupport;
+    /**
+     * Returns If User exists or not.
+     *
+     * @param String $id
+     * @return mixed
+     */
+    public function clientGymSubscriptionExists($id)
+    {
+        $clientGymSubscribedPlan =  DB::table('clients_gym_subscriptions')
+            ->where('id', '=', $id)->first();
+        if (!$clientGymSubscribedPlan)
+            throw new ClientGymSubscriptionNotFound();
+        return $clientGymSubscribedPlan;
+    }
+    public function clientAlreadyOnPlan(User $user)
+    {
+        $subscribedPlan =  DB::table('clients_gym_subscriptions')
+            ->where('client_id', '=', $user->id)
+            ->where('end', '>=', Carbon::now())->first();
+        if ($subscribedPlan)
+            throw new ClientAlreadySubscribedToGymSubscription();
+    }
+}
