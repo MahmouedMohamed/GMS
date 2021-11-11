119527bc692d721dc2769fe01c1fe8d31923beaf
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-11-11 13:47:37 +0200

    Added named routes

--- routes/web.php
+++ routes/web.php
@@ -30,16 +30,59 @@ use Inertia\Inertia;
 //     ]);
 // });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/',[HomeController::class,'index']);
-Route::get('/admin', [AdminController::class, 'generalAdminDashboard']);
-Route::apiResource('/gym/subscriptions',GymSubscriptionPlanController::class);
-Route::apiResource('/gym/subscribe',ClientGymSubscriptionController::class);
-Route::apiResource('/trainer/subscriptions',TrainerSubscriptionPlanController::class);
-Route::apiResource('/trainer/subscribe',ClientTrainerSubscriptionController::class);
-Route::apiResource('/trainer/shifts',TrainerShiftController::class);
-Route::get('/trainer/{id}/shifts',[TrainerShiftController::class,'getSpecificTrainerShifts']);
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get('/admin', [AdminController::class, 'generalAdminDashboard']);
 
-require __DIR__.'/auth.php';
+//To Make URL Changes Dynamically without affecting Views
+Route::resource('/gym/subscriptions', GymSubscriptionPlanController::class)->names([
+    'index' => 'gym.subscriptions.index',
+    'show' => 'gym.subscriptions.show',
+    'create' => 'gym.subscriptions.create',
+    'store' => 'gym.subscriptions.store',
+    'edit' => 'gym.subscriptions.edit',
+    'update' => 'gym.subscriptions.update',
+    'destroy' => 'gym.subscriptions.destroy',
+]);
+Route::resource('/gym/subscribe', ClientGymSubscriptionController::class)->names([
+    'index' => 'gym.subscribe.index',
+    'show' => 'gym.subscribe.show',
+    'create' => 'gym.subscribe.create',
+    'store' => 'gym.subscribe.store',
+    'edit' => 'gym.subscribe.edit',
+    'update' => 'gym.subscribe.update',
+    'destroy' => 'gym.subscribe.destroy',
+]);
+Route::resource('/trainer/subscriptions', TrainerSubscriptionPlanController::class)->names([
+    'index' => 'trainer.subscriptions.index',
+    'show' => 'trainer.subscriptions.show',
+    'create' => 'trainer.subscriptions.create',
+    'store' => 'trainer.subscriptions.store',
+    'edit' => 'trainer.subscriptions.edit',
+    'update' => 'trainer.subscriptions.update',
+    'destroy' => 'trainer.subscriptions.destroy',
+]);
+Route::resource('/trainer/subscribe', ClientTrainerSubscriptionController::class)->names([
+    'index' => 'trainer.subscribe.index',
+    'show' => 'trainer.subscribe.show',
+    'create' => 'trainer.subscribe.create',
+    'store' => 'trainer.subscribe.store',
+    'edit' => 'trainer.subscribe.edit',
+    'update' => 'trainer.subscribe.update',
+    'destroy' => 'trainer.subscribe.destroy',
+]);
+Route::resource('/trainer/shifts', TrainerShiftController::class)->names([
+    'index' => 'trainer.shifts.index',
+    'show' => 'trainer.shifts.show',
+    'create' => 'trainer.shifts.create',
+    'store' => 'trainer.shifts.store',
+    'edit' => 'trainer.shifts.edit',
+    'update' => 'trainer.shifts.update',
+    'destroy' => 'trainer.shifts.destroy',
+]);
+Route::get('/trainer/{id}/shifts', [TrainerShiftController::class, 'getSpecificTrainerShifts'])
+    ->name('trainer.shifts.self');
+
+require __DIR__ . '/auth.php';
