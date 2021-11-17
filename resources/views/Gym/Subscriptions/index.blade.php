c7e66de86b2bb2d524e2575c934c08de11186e06
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-11-17 16:32:28 +0200

    Update Gym Subscriptions Plan Index Page

--- resources/views/Gym/Subscriptions/index.blade.php
+++ resources/views/Gym/Subscriptions/index.blade.php
@@ -1,13 +1,43 @@
-@if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
+@extends('layouts.app')
+@section('content')
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="container">
+        <div class="row row-cols-3 row-cols-md-2 g-4">
+            @forelse ($plans as $plan)
+                <div class="col row pr-0 m-1">
+                    <div class="card text-white bg-dark">
+                        <img src="https://picsum.photos/200/300" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ $plan->Name }}</h5>
+                            <p class="card-text">Enjoy {{ $plan->NumberofMonths }}
+                                {{ $plan->NumberofMonths > 1 ? 'Months' : 'Month' }} of with only
+                                {{ $plan->Cost }} EGP.</p>
+                            <div class="row justify-content-around">
+                                <a href="{{ route('gym.subscriptions.edit', ['subscription' => $plan->ID]) }}"
+                                    class="btn btn-primary ">Edit</a>
+                                <form action="{{ route('gym.subscriptions.destroy', ['subscription' => $plan->ID]) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                        @if ($plan->HasDiscount)
+                        <div class="bg-red-500 text-white position-absolute top-0 right-0">Discount</div>
+                    @endif
+                    </div>
+
+                </div>
+            @empty
+                <p>No Plans</p>
+            @endforelse
+        </div>
     </div>
-@endif
-<div>
-    @forelse ($plans as $plan)
-        <li>{{ $plan->name }}</li>
-    @empty
-        <p>No Plans</p>
-    @endforelse
-</div>
-<a href="{{ route('gym.subscriptions.create') }}">Create New</a>
+    <a href="{{ route('gym.subscriptions.create') }}">Create New</a>
+@endsection
